2687cd6657541e3bbb51c12a05e828b2e15e3cd5
HIMANSHU6001 <hk9797592893@gmail.com>
2024-05-27 03:55:39 -0700

    logging uri

--- server-side/db.js
+++ server-side/db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 
 async function connectToMongo() {
-    console.log("Connecting to Mongo.....");
+    console.log("Connecting to Mongo.....", process.env.URI);
     await mongoose.connect(process.env.URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
