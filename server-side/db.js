c292fdccff18b58cc17720aac17b5b3dbcd78f2e
HIMANSHU6001 <hk9797592893@gmail.com>
2024-05-27 03:33:36 -0700

    changed connection

--- server-side/db.js
+++ server-side/db.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 
 async function connectToMongo() {
     console.log("Connecting to Mongo.....");
-    await mongoose.connect(process.env.URI);
+    await mongoose.connect(process.env.URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      })
     console.log("Connected to Mongo");
     }
 
